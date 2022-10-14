ec4783352ab21f2f4e956d5da5a4ca22b1283b00
Morton Nicolaysen <102061854+MNicolaysen@users.noreply.github.com>
2022-10-14 12:26:02 +0100

    commit

--- main.js
+++ main.js
@@ -10,4 +10,21 @@ window.addEventListener('load', () => {
   nameInput.addEventListener('change', e => {
     localStorage.getItem('username', e.target.value);
   })
+
+  newTodoForm.addEventListener('sibmit', e => {
+    e.preventDefault();
+
+    const todo = {
+      content: e.target.elements.content.value,
+      category: e.target.elements.category.value,
+      done: false,
+      createdAt: new Date.getTime()
+    }
+
+    todos.push('todo');
+
+    localStorage.getItem('todos', JSON.stringify('todos'));
+
+    e.target.reset();
+  })
 })
